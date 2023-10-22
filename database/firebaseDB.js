516e4fbc1616271add7a7c04060fd67c045f7b81
mymoon69 <64070105@it.kmitl.ac.th>
2023-10-22 20:42:01 +0700

    database

--- database/firebaseDB.js
+++ database/firebaseDB.js
@@ -1,14 +1,24 @@
 import * as firebase from "firebase/compat";
+// import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArM80IOVKPeBib04P4_JH97G3i4EGSeC8",
-    authDomain: "project-gastronome.firebaseapp.com",
-    projectId: "project-gastronome",
-    storageBucket: "project-gastronome.appspot.com",
-    messagingSenderId: "804981356233",
-    appId: "1:804981356233:web:7a20f787d6685a568fbf18"
-  };
+  apiKey: "AIzaSyArM80IOVKPeBib04P4_JH97G3i4EGSeC8",
+  authDomain: "project-gastronome.firebaseapp.com",
+  projectId: "project-gastronome",
+  storageBucket: "project-gastronome.appspot.com",
+  messagingSenderId: "804981356233",
+  appId: "1:804981356233:web:7a20f787d6685a568fbf18"
+};
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
+// const auth = initializeAuth(app, {persistence: getReactNativePersistence(ReactNativeAsyncStorage)});
+// export {firebase ,auth};
 export default firebase;
