ddc0ad893dfa1cbd6df5fef3dbf9b008fb7a67df
mymoon69 <64070105@it.kmitl.ac.th>
2023-10-24 00:10:38 +0700

    ดีเทลนิดนึง

--- screen/MainScreen/DetailScreen.js
+++ screen/MainScreen/DetailScreen.js
@@ -1,168 +1,213 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, StyleSheet, Image, ScrollView, TextInput, Pressable } from "react-native";
 import { FontAwesome, Fontisto } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import firebase from "../../database/firebaseDB";
 
 import MenuCarousel from '../../components/MenuCarousel'
 import Buttonseecomment from '../../components/Buttonseecomment';
 import { Button, Input } from "react-native-elements";
 
-const DetailSceen = () => {
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                <Image source={{
-                    uri: "https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/25/2022/10/tom-yum-recipe.jpg"
-                }}
-                    style={styles.Image}
-                />
-                <Text style={styles.name}>ตามสั่งคุณสมัย</Text>
-                <View style={styles.rating}>
-                    <View style={styles.star}>
-                        <Text style={{ color: 'white', textAlign: 'center' }}>4.5</Text>
-                        <FontAwesome name="star" size={15} color="white" />
+class DetailSceen extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            key: "",
+            id: "",
+            name: "",
+            picture: "",
+            detail: "",
+            rating: "",
+            review: "",
+            telephone: "",
+            category_id: "",
+            category_name: "",
+        };
+    }
+
+    componentDidMount() {
+        const ResDoc = firebase.firestore().collection("Restaurant").doc(this.props.route.params.key);
+        ResDoc.get().then((res) => {
+            if (res.exists) {
+                const Res = res.data();
+                this.setState({
+                    key: res.id,
+                    id: Res.id,
+                    name: Res.name,
+                    picture: Res.picture,
+                    detail: Res.detail,
+                    rating: Res.rating,
+                    review: Res.review,
+                    telephone: Res.telephone,
+                    category_id: Res.category_id,
+                    category_name: Res.category_name,
+                });
+            } else {
+                console.log("Doesn't has Restaurant")
+            }
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView>
+                    <Image source={{
+                        uri: this.state.picture
+                    }}
+                        style={styles.Image}
+                    />
+                    <Text style={styles.name}>{this.state.name}</Text>
+                    <View style={styles.rating}>
+                        <View style={styles.star}>
+                            <Text style={{ color: 'white', textAlign: 'center' }}>{this.state.rating}</Text>
+                            <FontAwesome name="star" size={15} color="white" />
+                        </View>
+
+                        <Text>{this.state.review} รีวิว</Text>
+
+                        <View style={styles.fav}>
+                            <Fontisto name="favorite" size={24} color="black" />
+                            <Text>ชื่นชอบ</Text>
+                        </View>
                     </View>
+                    <Text style={{ marginTop: 10, color: 'gray' }}>{this.state.category_name}</Text>
+                    <Text style={{ fontWeight: 'bold', marginTop: 10 }}>รายละเอียด</Text>
+                    <Text style={{ marginTop: 10 }}>{this.state.detail}</Text>
+                    <Text>โทร : {this.state.telephone}</Text>
+                    <Image source={{
+                        uri: "https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/25/2022/10/tom-yum-recipe.jpg"
+                    }}
+                        style={{ width: 380, height: 86, borderRadius: 20, marginTop: 10 }}
+                    />
 
-                    <Text>10 เรตติ้ง</Text>
+                    <Text style={styles.menurec}>เมนูที่ร้านแนะนำ {'>'}</Text>
+                    <MenuCarousel/>
+                    <Text style={styles.commant}>เพิ่มความคิดเห็น</Text>
 
-                    <View style={styles.fav}>
-                        <Fontisto name="favorite" size={24} color="black" />
-                        <Text>ชื่นชอบ</Text>
-                    </View>
-                </View>
-                <Text style={{ marginTop: 10, color: 'gray' }}>อาหารตามสั่ง</Text>
-                <Text style={{ fontWeight: 'bold', marginTop: 10 }}>รายละเอียด</Text>
-                <Text style={{ marginTop: 10 }}>qqwertyuiop[]asdkl;'zxcvbnm,./qawexrctvgbynmk,lsxdcfvgbhnjmk,ps fghjkl</Text>
-                <Image source={{
-                    uri: "https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/25/2022/10/tom-yum-recipe.jpg"
-                }}
-                    style={{ width: 380, height: 86, borderRadius: 20, marginTop: 10 }}
-                />
-
-                <Text style={styles.menurec}>เมนูที่ร้านแนะนำ {'>'}</Text>
-                <Text style={styles.commant}>เพิ่มความคิดเห็น</Text>
-
-                <View style={styles.addComment}>
-                    <View style={{ flexDirection: 'row', }}>
-                        <Image
-                            source={require("../../assets/3135823.png")}
-                            style={{ width: 40, height: 40, marginLeft: 10 }}
-                        />
-                        <View style={{ marginBottom: 15, marginTop: 25, }}>
-                            <View style={{ marginTop: -20, marginLeft: 20, }}>
-                                <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
+                    <View style={styles.addComment}>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Image
+                                source={require("../../assets/3135823.png")}
+                                style={{ width: 40, height: 40, marginLeft: 10 }}
+                            />
+                            <View style={{ marginBottom: 15, marginTop: 25, }}>
+                                <View style={{ marginTop: -20, marginLeft: 20, }}>
+                                    <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginLeft: 15, }}>
+                                    <AntDesign name="staro" size={24} color="orange" />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                </View>
                             </View>
-                            <View style={{ flexDirection: 'row', marginLeft: 15, }}>
+                        </View>
+
+                        <TextInput style={styles.txtInput} placeholder={"พิมพ์ความคิดเห็นที่นี่..."}>
+                        </TextInput>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Pressable style={styles.upImg} >
+                                {/* ใส่ onPress={onPress} ใน Pressable*/}
                                 <AntDesign name="staro" size={24} color="orange" />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
-                            </View>
+                            </Pressable>
+                            <Pressable style={styles.submit} >
+                                {/* ใส่ onPress={onPress} ใน Pressable*/}
+                                <Text>ส่งความคิดเห็น</Text>
+                            </Pressable>
                         </View>
                     </View>
 
-                    <TextInput style={styles.txtInput} placeholder={"พิมพ์ความคิดเห็นที่นี่..."}>
-                    </TextInput>
                     <View style={{ flexDirection: 'row', }}>
-                        <Pressable style={styles.upImg} >
-                            {/* ใส่ onPress={onPress} ใน Pressable*/}
-                            <AntDesign name="staro" size={24} color="orange" />
-                        </Pressable>
-                        <Pressable style={styles.submit} >
-                            {/* ใส่ onPress={onPress} ใน Pressable*/}
-                            <Text>ส่งความคิดเห็น</Text>
-                        </Pressable>
+                        <Text style={styles.commant}>ความคิดเห็น</Text>
+                        <Text style={styles.commant}>(10)</Text>
                     </View>
-                </View>
-
-                <View style={{ flexDirection: 'row', }}>
-                    <Text style={styles.commant}>ความคิดเห็น</Text>
-                    <Text style={styles.commant}>(10)</Text>
-                </View>
-                <View style={styles.restComment}>
-                    <View style={{ flexDirection: 'row', }}>
-                        <Image
-                            source={require("../../assets/3135823.png")}
-                            style={{ width: 40, height: 40, marginLeft: 10 }}
-                        />
-                        <View style={{ marginBottom: 15, marginTop: 25, }}>
-                            <View style={{ marginTop: -20, marginLeft: 20, }}>
-                                <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
-                            </View>
-                            <View style={{ flexDirection: 'row', marginLeft: 15, }}>
-                                <AntDesign name="star" size={24} color="orange" />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                    <View style={styles.restComment}>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Image
+                                source={require("../../assets/3135823.png")}
+                                style={{ width: 40, height: 40, marginLeft: 10 }}
+                            />
+                            <View style={{ marginBottom: 15, marginTop: 25, }}>
+                                <View style={{ marginTop: -20, marginLeft: 20, }}>
+                                    <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginLeft: 15, }}>
+                                    <AntDesign name="star" size={24} color="orange" />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                </View>
                             </View>
                         </View>
-                    </View>
-                    <Text style={{width: 360}}>
-                    ร้านนี้อยู่ปากซอยลาดกระบัง 14 ( อยู่ตรงทางยูเทิร์น ) เป็นร้านขนาดกลาง ๆ มีที่นั่งทานพอสมควร ไม่แน่ใจว่าถ้าขับรถมาต้องไปจอดตรงไหน และ ร้านนี้ไม่มีบริการโอน มีแต่เงินสดเท่านั้น อาหารอร่อยมากเลย อารมณ์เหมือนกินกับข้าวที่แม่ทำ
-                    </Text>
-                    <View style={{ flexDirection: 'row', }}>
-                    <Image
-                            source={require("../../assets/original-1593006369515.jpg")}
-                            style={{ width: 150, height: 130, marginLeft: 10, marginTop:10 }}
-                        />
-                        <Image
-                            source={require("../../assets/original-1593006369515.jpg")}
-                            style={{ width: 150, height: 130, marginLeft: 10, marginTop:10 }}
-                        />
-                    </View>
-                    <Pressable style={styles.allSubmit} >
+                        <Text style={{ width: 360 }}>
+                            ร้านนี้อยู่ปากซอยลาดกระบัง 14 ( อยู่ตรงทางยูเทิร์น ) เป็นร้านขนาดกลาง ๆ มีที่นั่งทานพอสมควร ไม่แน่ใจว่าถ้าขับรถมาต้องไปจอดตรงไหน และ ร้านนี้ไม่มีบริการโอน มีแต่เงินสดเท่านั้น อาหารอร่อยมากเลย อารมณ์เหมือนกินกับข้าวที่แม่ทำ
+                        </Text>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Image
+                                source={require("../../assets/original-1593006369515.jpg")}
+                                style={{ width: 150, height: 130, marginLeft: 10, marginTop: 10 }}
+                            />
+                            <Image
+                                source={require("../../assets/original-1593006369515.jpg")}
+                                style={{ width: 150, height: 130, marginLeft: 10, marginTop: 10 }}
+                            />
+                        </View>
+                        <Pressable style={styles.allSubmit} >
                             {/* ใส่ onPress={onPress} ใน Pressable*/}
                             <Text>แสดงความคิดเห็น</Text>
                         </Pressable>
-                </View>
+                    </View>
 
-                <View style={styles.restComment}>
-                    <View style={{ flexDirection: 'row', }}>
-                        <Image
-                            source={require("../../assets/3135823.png")}
-                            style={{ width: 40, height: 40, marginLeft: 10 }}
-                        />
-                        <View style={{ marginBottom: 15, marginTop: 25, }}>
-                            <View style={{ marginTop: -20, marginLeft: 20, }}>
-                                <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
-                            </View>
-                            <View style={{ flexDirection: 'row', marginLeft: 15, }}>
-                                <AntDesign name="star" size={24} color="orange" />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
-                                <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                    <View style={styles.restComment}>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Image
+                                source={require("../../assets/3135823.png")}
+                                style={{ width: 40, height: 40, marginLeft: 10 }}
+                            />
+                            <View style={{ marginBottom: 15, marginTop: 25, }}>
+                                <View style={{ marginTop: -20, marginLeft: 20, }}>
+                                    <Text style={{ fontWeight: 'bold', }}>Manee memsew</Text>
+                                </View>
+                                <View style={{ flexDirection: 'row', marginLeft: 15, }}>
+                                    <AntDesign name="star" size={24} color="orange" />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="star" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                    <AntDesign name="staro" size={24} color="orange" style={{ marginLeft: 5, }} />
+                                </View>
                             </View>
                         </View>
-                    </View>
-                    <Text style={{width: 360}}>
-                    ร้านนี้อยู่ปากซอยลาดกระบัง 14 ( อยู่ตรงทางยูเทิร์น ) เป็นร้านขนาดกลาง ๆ มีที่นั่งทานพอสมควร ไม่แน่ใจว่าถ้าขับรถมาต้องไปจอดตรงไหน และ ร้านนี้ไม่มีบริการโอน มีแต่เงินสดเท่านั้น อาหารอร่อยมากเลย อารมณ์เหมือนกินกับข้าวที่แม่ทำ
-                    </Text>
-                    <View style={{ flexDirection: 'row', }}>
-                    <Image
-                            source={require("../../assets/original-1593006369515.jpg")}
-                            style={{ width: 150, height: 130, marginLeft: 10, marginTop:10 }}
-                        />
-                        <Image
-                            source={require("../../assets/original-1593006369515.jpg")}
-                            style={{ width: 150, height: 130, marginLeft: 10, marginTop:10 }}
-                        />
-                    </View>
-                    <Pressable style={styles.allSubmit} >
+                        <Text style={{ width: 360 }}>
+                            ร้านนี้อยู่ปากซอยลาดกระบัง 14 ( อยู่ตรงทางยูเทิร์น ) เป็นร้านขนาดกลาง ๆ มีที่นั่งทานพอสมควร ไม่แน่ใจว่าถ้าขับรถมาต้องไปจอดตรงไหน และ ร้านนี้ไม่มีบริการโอน มีแต่เงินสดเท่านั้น อาหารอร่อยมากเลย อารมณ์เหมือนกินกับข้าวที่แม่ทำ
+                        </Text>
+                        <View style={{ flexDirection: 'row', }}>
+                            <Image
+                                source={require("../../assets/original-1593006369515.jpg")}
+                                style={{ width: 150, height: 130, marginLeft: 10, marginTop: 10 }}
+                            />
+                            <Image
+                                source={require("../../assets/original-1593006369515.jpg")}
+                                style={{ width: 150, height: 130, marginLeft: 10, marginTop: 10 }}
+                            />
+                        </View>
+                        <Pressable style={styles.allSubmit} >
                             {/* ใส่ onPress={onPress} ใน Pressable*/}
                             <Text>แสดงความคิดเห็น</Text>
                         </Pressable>
-                </View>
+                    </View>
 
 
-                <Buttonseecomment />
-            </ScrollView>
+                    <Buttonseecomment />
+                </ScrollView>
 
 
-        </View>
-    );
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -256,7 +301,7 @@ const styles = StyleSheet.create({
         padding: 10,
         height: 360,
         width: 380,
-        marginBottom:15,
+        marginBottom: 15,
     },
     allSubmit: {
         backgroundColor: "#FFDBAA",
